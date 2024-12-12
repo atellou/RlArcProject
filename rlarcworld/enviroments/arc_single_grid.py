759db89e1e1352a5ac19760a86e113da634fba2d
atellou <andresfe.t.u@gmail.com>
2024-12-12 18:11:04 -0500

    [BUILD] Folders Name changes

--- rlarcworld/enviroments/arc_single_grid.py
+++ rlarcworld/enviroments/arc_single_grid.py
@@ -7,26 +7,21 @@ logger = logging.getLogger(__name__)
 
 
 class ArcSingleGrid(gym.Env):
+    # Possibly multiple test grids.
+    # Return two samples to evaluate.
 
-    def __init__(self, arc_dataset_dir: str, size: int = 30, color_values: int = 9):
-        self.arc_dir = arc_dataset_dir
-
-        # The maximum size of the square grid
-        self.size = size
+    def __init__(self, max_size: int, color_values: int = 9):
         # Color value extra for resizing
         self.color_values = color_values + 1
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`-1}^2
-        self.observation_space = gym.spaces.Dict(
-            {
-                "current": gym.spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": gym.spaces.Box(0, size - 1, shape=(2,), dtype=int),
-            }
-        )
+        self.observation_space = gym.spaces.MultiDiscrete([max_size, max_size])
 
         # We have actions corresponding to "X Location", "Y Location", "Color Value" and "submission"
-        self.action_space = gym.spaces.MultiDiscrete([size, size, color_values, 1])
+        self.action_space = gym.spaces.MultiDiscrete(
+            [max_size, max_size, color_values, 1]
+        )
 
     def _get_obs(self):
         return {"current": self._current_grid, "target": self._target_grid}
@@ -39,19 +34,18 @@ class ArcSingleGrid(gym.Env):
 
     def random_grid_generator(self):
         """
-        Generate a random grid within the max size and color values parameters
+        Generate a random location in the grid
         """
-        # TODO: Get sample from dataset. Currently just random grid.
-        return self.np_random.integers(
-            0, self.color_values, size=(self.size, self.size), dtype=int
-        )
+        return tuple(self.observation_space.sample())
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
-        self._reward_storage = 0
+
+        batch = options["batch"]
 
         # State of initial reward
+        self._reward_storage = 0
         self._last_reward = self._compute_reward()
 
         # Get initial grid
@@ -73,8 +67,19 @@ class ArcSingleGrid(gym.Env):
 
         return observation, info
 
-    def _compute_reward(self):
-        return -1 * np.sum(np.abs(self._current_grid - self._target_grid))
+    def compute_pixel_correctnes(self):
+        return np.sum(np.abs(self._current_grid - self._target_grid))
+
+    def reward(self, terminated: bool):
+        reward = -1 * int(not terminated)
+        if terminated:
+            logger.info("Successfully completed environment.")
+            # Maximum reward: expected to encourage exploration
+            reward = abs(self._reward_storage)
+        elif reward != 0 and submission:
+            logger.warning("Attempting to submit with reward equal {}.".format(reward))
+            # Maximum penalization
+            reward = -1 * self.size * self.size * self.color_values
 
     def step(self, action):
 
@@ -94,16 +99,9 @@ class ArcSingleGrid(gym.Env):
             )
 
         # An environment is completed if and only if the grids overlap
-        reward = self._compute_reward()
-        terminated = reward == 0 and submission
-        if terminated:
-            logger.info("Successfully completed environment.")
-            # Maximum reward: expected to encourage exploration
-            reward = abs(self._reward_storage)
-        elif reward != 0 and submission:
-            logger.warning("Attempting to submit with reward equal {}.".format(reward))
-            # Maximum penalization
-            reward = -1 * self.size * self.size * self.color_values
+        # np.sum(np.abs(self._current_grid - self._target_grid))
+        terminated = bool(submission)
+        reward = self.reward(terminated)
 
         self._reward_storage += reward
         self._last_reward = reward
