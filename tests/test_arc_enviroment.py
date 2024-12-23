fe4c897142e3fbe4d4c1881a6f01f60e3cb30f0b
atellou <andresfe.t.u@gmail.com>
2024-12-22 21:19:39 -0500

    [TEST] Addition of pixel wise wrapper tests, merge with other tests

--- tests/test_arc_enviroment.py
+++ tests/test_arc_enviroment.py
@@ -1,44 +1,60 @@
 from rlarcworld.enviroments.arc_batch_grid_env import ArcBatchGridEnv
+from rlarcworld.enviroments.wrappers.rewards import PixelAwareRewardWrapper
 import numpy as np
 import torch
 import unittest
 import logging
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info("Testing ArcBatchGridEnv")
 
 
 class ArcBatchGridsEnv(unittest.TestCase):
-    def test_batch(self):
-        batch_size = np.random.randint(1, 50)
-        size = np.random.randint(1, 50)
-        color_values = np.random.randint(1, 50)
-        logger.info(
-            "Testing Episodes with random batches. Batch size: {}, Grid Size: {}, Color values: {}".format(
-                batch_size, size, color_values
-            )
-        )
-        env = ArcBatchGridEnv(size=size, color_values=color_values)
-        for episode in range(3):
-            logger.info("Episode: {}".format(episode))
+
+    def test_environment(self):
+        logger.info("Testing ArcBatchGridEnv")
+        batch_size = np.random.randint(20, 50)
+        size = np.random.randint(20, 50)
+        color_values = np.random.randint(2, 20)
+
+        env = ArcBatchGridEnv(size, color_values)
+        self.episodes_simulation(batch_size, size, color_values, env)
+
+    def test_wrapper(self):
+        logger.info("\nTesting ArcBatchGridEnv with wrapper PixelAwareRewardWrapper")
+        batch_size = np.random.randint(20, 50)
+        size = np.random.randint(20, 50)
+        color_values = np.random.randint(2, 20)
+
+        env = ArcBatchGridEnv(size, color_values)
+        env = PixelAwareRewardWrapper(env)
+        self.episodes_simulation(batch_size, size, color_values, env)
+
+    def episodes_simulation(self, batch_size, size, color_values, env):
+        logger.info("Batch size: {}".format(batch_size))
+        logger.info("Grid size: {}".format(size))
+        logger.info("Color values: {}".format(color_values))
+        episodes = np.random.randint(5, 10)
+        for episode in range(episodes):
+            logger.info("Episode: {}/{}".format(episode, episodes))
             dummy_batch = {
                 "batch": {
                     "input": torch.randint(
-                        0, color_values, size=(batch_size, size, size)
+                        0, color_values - 1, size=(batch_size, size, size)
                     ),
                     "output": torch.randint(
                         0, color_values, size=(batch_size, size, size)
                     ),
                 }
             }
+            # dummy_batch["batch"]["output"] = dummy_batch["batch"]["input"].clone() + 1
             env.reset(dummy_batch)
-            initial_diff_grid = torch.abs(
-                dummy_batch["batch"]["input"] - dummy_batch["batch"]["output"]
-            )
+            initial_diff_grid = env.get_difference()
             last_diff = torch.sum(initial_diff_grid)
             step = 0
             for y_loc in range(size):
                 for x_loc in range(size):
+                    logger.debug(f"\n>>>>>>>Step {step}/{size**2} <<<<<<<<<<<")
                     target_grids = env.observations["target"]
                     is_last_step = int(y_loc == size - 1 and x_loc == size - 1)
                     value = target_grids[:, y_loc, x_loc].numpy(force=False)
@@ -53,11 +69,12 @@ class ArcBatchGridsEnv(unittest.TestCase):
                     assert env.action_space.contains(action), ValueError(
                         "Action not in action space: {}".format(action)
                     )
-                    last_diff = torch.sum(torch.abs(env.get_difference()))
+                    last_diff = env.get_difference()
                     observation, reward, terminated, truncated, info = env.step(action)
-                    current_diff = torch.sum(torch.abs(env.get_difference()))
+                    current_diff = env.get_difference()
+
                     step += 1
-                    logger.info(
+                    logger.debug(
                         f"Computing differences in grids for the current step [{step}]"
                     )
                     sum_changed_values, sum_init_diff_values = self.compute_difference(
@@ -65,7 +82,7 @@ class ArcBatchGridsEnv(unittest.TestCase):
                         last_diff,
                         initial_diff_grid[:, y_loc, x_loc],
                     )
-                    logger.info(f"Assertions of grids")
+                    logger.debug(f"Assertions of grids")
                     self.assertions_grids(
                         batch_size,
                         step,
@@ -77,15 +94,41 @@ class ArcBatchGridsEnv(unittest.TestCase):
                         sum_init_diff_values,
                         dummy_batch,
                     )
-                    logger.info(f"Assertions of rewards and termination")
-                    self.assertions_reward_termination(
-                        reward,
-                        observation,
-                        submission,
-                        terminated,
-                        is_last_step,
-                        current_diff,
-                    )
+                    logger.debug(f"Assertions of termination")
+                    current_diff = torch.sum(torch.abs(current_diff))
+                    if is_last_step:
+                        assert terminated, "The last step should terminate the episode"
+                        assert (
+                            current_diff == 0
+                        ), "The last step should have zero difference"
+                    else:
+                        assert not terminated or (
+                            terminated and current_diff == 0
+                        ), "The episode should not terminate"
+
+                    logger.debug(f"Assertions of rewards")
+                    if isinstance(env, PixelAwareRewardWrapper):
+                        self.assert_reward_pixel(
+                            reward,
+                            observation,
+                            submission,
+                            is_last_step,
+                            current_diff,
+                            (
+                                dummy_batch["batch"]["input"][:, y_loc, x_loc]
+                                - dummy_batch["batch"]["output"][:, y_loc, x_loc]
+                                != 0
+                            ).long(),
+                            size,
+                            color_values,
+                        )
+                    elif isinstance(env, ArcBatchGridEnv):
+                        self.assert_reward_arc(
+                            reward,
+                            observation,
+                            submission,
+                            is_last_step,
+                        )
 
     def compute_difference(self, current_diff, last_diff, initial_diff_grid_loc):
         """
@@ -99,8 +142,8 @@ class ArcBatchGridsEnv(unittest.TestCase):
             torch.Tensor: The change since the last action, improvement in grid differences.
             torch.Tensor: The sum of the initial difference values in the locations changed on the last action.
         """
-        sum_changed_values = torch.abs(last_diff - current_diff)
-        sum_init_diff_values = torch.sum(initial_diff_grid_loc)
+        sum_changed_values = torch.sum(torch.abs(last_diff - current_diff))
+        sum_init_diff_values = torch.sum(torch.abs(initial_diff_grid_loc))
         return sum_changed_values, sum_init_diff_values
 
     def assertions_grids(
@@ -147,8 +190,12 @@ class ArcBatchGridsEnv(unittest.TestCase):
             step, x_loc, y_loc
         )
 
-    def assertions_reward_termination(
-        self, reward, observation, submission, terminated, is_last_step, current_diff
+    def assert_reward_arc(
+        self,
+        reward,
+        observation,
+        submission,
+        is_last_step,
     ):
         """
         Assertions for the termination and rewards.
@@ -156,7 +203,6 @@ class ArcBatchGridsEnv(unittest.TestCase):
             reward (torch.Tensor): The reward for the current step.
             observation (dict): The observation for the current step.
             submission (torch.Tensor): A tensor indicating the submit action.
-            terminated (bool): A boolean indicating if the env returned termination.
             is_last_step (bool): A boolean indicating if the current step is the last step.
         """
 
@@ -164,8 +210,6 @@ class ArcBatchGridsEnv(unittest.TestCase):
             assert torch.equal(
                 reward, torch.ones_like(reward)
             ), "The last step should have a reward of 1"
-            assert terminated, "The last step should terminate the episode"
-            assert current_diff == 0, "The last step should have zero difference"
         else:
             assert torch.equal(
                 reward,
@@ -178,12 +222,50 @@ class ArcBatchGridsEnv(unittest.TestCase):
                 ).long()
                 * submission,
             ), "The step should have a reward of 0"
-            assert not terminated, "The episode should not terminate"
+
+    def assert_reward_pixel(
+        self,
+        reward,
+        observation,
+        submission,
+        is_last_step,
+        current_diff,
+        change,
+        size,
+        color_values,
+    ):
+        """
+        Assertions for the termination and rewards.
+        Args:
+            reward (torch.Tensor): The reward for the current step.
+            observation (dict): The observation for the current step.
+            submission (torch.Tensor): A tensor indicating the submit action.
+            terminated (bool): A boolean indicating if the env returned termination.
+            is_last_step (bool): A boolean indicating if the current step is the last step.
+            current_diff (torch.Tensor): The current difference between the current grid and the target grid.
+            change (int): The total change in the grid.
+            size (int): The size of the grid.
+            color_values (int): The number of color values.
+        """
+        if is_last_step:
+            assert torch.equal(
+                reward, torch.zeros_like(reward)
+            ), "The last step should have a reward of 0"
+        else:
+            current_diff = torch.sum(
+                ((observation["target"] - observation["current"]) != 0).long(),
+                dim=(1, 2),
+            )
+            reference = (change - 1) * (current_diff != 0).long()
+            assert torch.equal(
+                reward,
+                reference + (-1 * current_diff * submission * size**2 * color_values),
+            ), "The step do not have the expected reward."
 
     def test_action_space(self):
         grid_size = np.random.randint(5, 20)
         values = np.random.randint(2, 20)
-        logger.info(
+        logger.debug(
             "Testing Action Space. Grid Size: {}, Color values: {}".format(
                 grid_size, values
             )
