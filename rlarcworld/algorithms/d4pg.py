fix: Apply softmax to generate probability distribution from the projection. Test critic loss

rlarcworld/algorithms/d4pg.py
@@ -59,6 +59,16 @@ class D4PG:
             next_state["actions"] = best_next_action
             best_next_q_dist = critic_target(next_state)
 
+        # Assert probability mass function
+        for key, dist in best_next_q_dist.items():
+            torch.testing.assert_close(
+                torch.sum(dist, dim=1), torch.ones(dist.shape[0])
+            ), f"Probability mass function not normalized for key: {key}"
+            assert torch.all(dist >= 0), f"Negative probability values for key: {key}"
+            assert torch.all(
+                dist <= 1
+            ), f"Probability values greater than 1 for key: {key}"
+
         # Project the distribution using the categorical projection
         target_dist = TensorDict(
             {
@@ -70,6 +80,7 @@ class D4PG:
                     v_min[key],
                     v_max[key],
                     num_atoms[key],
+                    apply_softmax=True,
                 )
                 for key in best_next_q_dist.keys()
             }
@@ -92,7 +103,7 @@ class D4PG:
         """
         # Get state-action value distribution from the critic
         state["actions"] = action
-        q_dist = critic(state)
+        q_dist = critic(state)  # Shape: (batch_size, num_atoms)
         # KL Divergence
         loss = TensorDict(
             {

rlarcworld/utils.py
@@ -1,7 +1,9 @@
 import torch
 
 
-def categorical_projection(next_q_dist, rewards, dones, gamma, v_min, v_max, num_atoms):
+def categorical_projection(
+    next_q_dist, rewards, dones, gamma, v_min, v_max, num_atoms, apply_softmax=True
+):
     """
     Projects the target distribution using the Bellman update.
 
@@ -35,4 +37,4 @@ def categorical_projection(next_q_dist, rewards, dones, gamma, v_min, v_max, num
     projected_dist.scatter_add_(dim=-1, index=l, src=next_q_dist * (u.float() - b))
     projected_dist.scatter_add_(dim=-1, index=u, src=next_q_dist * (b - l.float()))
 
-    return projected_dist
+    return torch.softmax(projected_dist, dim=-1) if apply_softmax else projected_dist