fix: Correction and test added bad normalization for Index grid feature.

rlarcworld/agent/actor.py
@@ -9,7 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 class ArcActorNetwork(nn.Module):
-    def __init__(self, size: int, color_values, test: bool = False):
+    def __init__(
+        self, size: int, color_values, test: bool = False, epsilon: float = 1e-6
+    ):
         super(ArcActorNetwork, self).__init__()
         self.test = test
         self.size = size
@@ -83,18 +85,20 @@ class ArcActorNetwork(nn.Module):
         state = state.clone()
         # Validate input
         self.input_val(state)
+
         # Brodcast the state
         for key, value in state.items():
             if key == "terminated":
                 pass
             if key == "index":
-                value = value / torch.max(value)
+                max_value = torch.max(value)
+                value = value.float() if max_value == 0 else value / max_value
             else:
                 value = self.scale_arc_grids(value)
-            logger.debug(f"Input {key} shape: {value.shape}")
             state[key] = self.inputs_layers[key](value)
             state[key] = torch.relu(state[key])
             state[key] = state[key].view(state[key].shape[0], -1)
+            assert not torch.isnan(state[key]).any(), f"NaN in {key} layer"
 
         # Concatenate flattned states
         state = torch.cat(
@@ -107,7 +111,7 @@ class ArcActorNetwork(nn.Module):
         state, _ = self.gru(state)
 
         if self.test:
-            output = TensorDict(
+            state = TensorDict(
                 {
                     reward_type: torch.softmax(
                         layer(state) * 0.1 + torch.linspace(0, 1, layer.out_features),
@@ -117,11 +121,17 @@ class ArcActorNetwork(nn.Module):
                 }
             )
         else:
-            output = TensorDict(
+            state = TensorDict(
                 {
                     reward_type: torch.softmax(layer(state), dim=-1)
                     for reward_type, layer in self.outputs_layers.items()
                 },
             )
-        self.output_val(output)
-        return output.auto_batch_size_(batch_dims=0)
+        self.output_val(state)
+
+        for key in state.keys():
+            assert not torch.isnan(
+                state[key]
+            ).any(), f"NaN detected in actor output for key {key}!"
+
+        return state.auto_batch_size_(batch_dims=0)

rlarcworld/agent/critic.py
@@ -121,7 +121,8 @@ class ArcCriticNetwork(torch.nn.Module):
             if key == "terminated":
                 pass
             if key == "index":
-                value = value / torch.max(value)
+                max_value = torch.max(value)
+                value = value.float() if max_value == 0 else value / max_value
             elif key == "actions":
                 # x_location, y_location, color_values, submit
                 value = value / torch.tensor(
@@ -132,6 +133,7 @@ class ArcCriticNetwork(torch.nn.Module):
             state[key] = self.inputs_layers[key](value)
             state[key] = torch.relu(state[key])
             state[key] = state[key].view(state[key].shape[0], -1)
+            assert not torch.isnan(state[key]).any(), f"NaN in {key} layer"
 
         # Concatenate flattned states
         state = torch.cat(
@@ -143,21 +145,21 @@ class ArcCriticNetwork(torch.nn.Module):
         state = self.linear1(state)
         state, _ = self.gru(state)
         if self.test:
-            output = TensorDict(
+            state = TensorDict(
                 {
-                    reward_type: torch.softmax(
+                    key: torch.softmax(
                         layer(state) * 0.1 + torch.linspace(0, 1, layer.out_features),
                         dim=-1,
                     )
-                    for reward_type, layer in self.outputs_layers.items()
+                    for key, layer in self.outputs_layers.items()
                 }
             )
         else:
-            output = TensorDict(
+            state = TensorDict(
                 {
-                    reward_type: torch.softmax(layer(state), dim=-1)
-                    for reward_type, layer in self.outputs_layers.items()
+                    key: torch.softmax(layer(state), dim=-1)
+                    for key, layer in self.outputs_layers.items()
                 }
             )
-        self.output_val(output)
-        return output.auto_batch_size_(batch_dims=0)
+
+        return state.auto_batch_size_(batch_dims=0)

tests/test_networks.py
@@ -97,8 +97,6 @@ class ArcNetworksTest(unittest.TestCase):
                 dc.pop(key)
                 network.input_val(dc)
 
-        # Forward pass
-        org_sample = input_sample.clone()
         action_probs = {
             "x_location": torch.softmax(torch.randn(self.batch_size, 30), dim=-1),
             "y_location": torch.softmax(torch.randn(self.batch_size, 30), dim=-1),
@@ -109,6 +107,15 @@ class ArcNetworksTest(unittest.TestCase):
             [torch.argmax(x, dim=-1).unsqueeze(-1) for x in action_probs.values()],
             dim=-1,
         ).float()
+
+        # numeric stability test
+        for key in input_sample.keys():
+            if torch.rand(1).item() < 0.2:
+                input_sample[key] = input_sample[key] * 0.0
+        if torch.rand(1).item() < 0.1:
+            best_next_action = best_next_action * 0.0
+        # Forward pass
+        org_sample = input_sample.clone()
         output = network(input_sample, action=best_next_action)
 
         # Validate not inplace changes to input
@@ -215,6 +222,10 @@ class ArcNetworksTest(unittest.TestCase):
                 dc.pop(key)
                 network.input_val(dc)
 
+        # numeric stability test
+        for key in input_sample.keys():
+            if torch.rand(1).item() < 0.2:
+                input_sample[key] = input_sample[key] * 0.0
         # Forward pass
         org_sample = input_sample.clone()
         output = network(input_sample)