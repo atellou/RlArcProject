fix: Correction to gradient validation for zero inputs. Isuue: #5

rlarcworld/agent/actor.py
@@ -88,12 +88,10 @@ class ArcActorNetwork(nn.Module):
 
         # Brodcast the state
         for key, value in state.items():
-            if key == "terminated":
-                pass
             if key == "index":
                 max_value = torch.max(value)
                 value = value.float() if max_value == 0 else value / max_value
-            else:
+            elif key != "terminated":
                 value = self.scale_arc_grids(value)
             state[key] = self.inputs_layers[key](value)
             state[key] = torch.relu(state[key])

rlarcworld/agent/critic.py
@@ -40,6 +40,13 @@ class ArcCriticNetwork(torch.nn.Module):
             key: torch.linspace(v_min[key], v_max[key], value)
             for key, value in num_atoms.items()
         }
+        self.no_scale_keys = [
+            "x_location",
+            "y_location",
+            "color_values",
+            "submit",
+            "terminated",
+        ]
         self.inputs_layers = torch.nn.ModuleDict(
             {
                 "last_grid": torch.nn.Conv2d(
@@ -60,6 +67,7 @@ class ArcCriticNetwork(torch.nn.Module):
                 "terminated": torch.nn.Linear(1, 1),
             }
         )
+
         self.linear1 = torch.nn.Linear(16469, 128)
         self.gru = torch.nn.GRU(
             input_size=128,
@@ -122,12 +130,10 @@ class ArcCriticNetwork(torch.nn.Module):
         state.update(action)
         # Brodcast the state
         for key, value in state.items():
-            if key == "terminated":
-                pass
             if key == "index":
                 max_value = torch.max(value)
                 value = value.float() if max_value == 0 else value / max_value
-            else:
+            elif key not in self.no_scale_keys:
                 value = self.scale_arc_grids(value)
             state[key] = self.inputs_layers[key](value)
             state[key] = torch.relu(state[key])

rlarcworld/algorithms/d4pg.py
@@ -467,12 +467,7 @@ class D4PG:
             self.n_steps <= max_steps or max_steps < 0
         ), "max_steps must be greater or equal to n_steps"
 
-        global_step = 0
-        episode = 0
-        cumulated_metrics = {}
-        for episode, sample_batch in enumerate(samples):
-            episode += 1
-            episode_step = 0
+        for episode_number, sample_batch in enumerate(samples):
 
             # episode_reward = {"pixel_wise": 0.0, "binary": 0.0}
             env.reset(
@@ -480,20 +475,12 @@ class D4PG:
                     "batch": sample_batch["task"],
                     "examples": sample_batch["examples"],
                 },
-                seed=episode,
+                seed=episode_number,
             )
             for step_state in self.episodes_simulation(
-                env, sample_batch, max_steps, seed=episode
+                env, sample_batch, max_steps, seed=episode_number
             ):
-                episode_step += 1
-                global_step += 1
-
-                for key, value in step_state["reward"].items():
-                    cumulated_metrics[key] = cumulated_metrics.setdefault(
-                        key, 0
-                    ) + torch.sum(value)
-                step_state["cumulated_metrics"] = cumulated_metrics
-                yield step_state
+                yield episode_number, step_state
 
     def validation_process(self, max_steps=-1):
         if max_steps == -1:
@@ -503,59 +490,55 @@ class D4PG:
                 " Meaning, that all grids should be compleated to end the process."
             )
         with torch.no_grad():
-            for step_state in self.env_simulation(
-                self.validation_env,
-                self.validation_samples,
-                max_steps=max_steps,
+            for step_number, (episode_number, step_state) in enumerate(
+                self.env_simulation(
+                    self.validation_env,
+                    self.validation_samples,
+                    max_steps=max_steps,
+                )
             ):
                 step_state["terminated"] = step_state["next_state"]["terminated"]
                 loss_actor, loss_critic = self.compute_loss(step_state, training=False)
 
     def fit(
         self,
+        epochs=1,
         max_steps=-1,
     ):
-        cumulated_metrics = {}
-        for step_number, step_state in enumerate(
-            self.env_simulation(
-                self.train_env,
-                self.train_samples,
-                max_steps=max_steps,
-            )
-        ):
-            # Store the experience in the replay buffer
-            if self.replay_buffer is not None:
-                batch = self.replay_buffer_step(
-                    step_state["state"],
-                    step_state["actions"],
-                    step_state["reward"],
-                    step_state["next_state"],
+        for epoch in range(epochs):
+            for step_number, (episode_number, step_state) in enumerate(
+                self.env_simulation(
+                    self.train_env,
+                    self.train_samples,
+                    max_steps=max_steps,
                 )
-            elif self.replay_buffer is None:
-                batch = TensorDict(
-                    {
-                        "state": step_state["state"],
-                        "actions": step_state["actions"],
-                        "reward": step_state["reward"],
-                        "next_state": step_state["next_state"],
-                        "terminated": step_state["next_state"]["terminated"],
-                    },
-                ).auto_batch_size_()
-            else:
-                return
+            ):
+                # Store the experience in the replay buffer
+                if self.replay_buffer is not None:
+                    batch = self.replay_buffer_step(
+                        step_state["state"],
+                        step_state["actions"],
+                        step_state["reward"],
+                        step_state["next_state"],
+                    )
+                elif self.replay_buffer is None:
+                    batch = TensorDict(
+                        {
+                            "state": step_state["state"],
+                            "actions": step_state["actions"],
+                            "reward": step_state["reward"],
+                            "next_state": step_state["next_state"],
+                            "terminated": step_state["next_state"]["terminated"],
+                        },
+                    ).auto_batch_size_()
+                else:
+                    return
 
-            if batch is None:
-                continue
+                if batch is None:
+                    continue
 
-            batch = self.fileter_compleated_state(batch)
-            loss_actor, loss_critic = self.compute_loss(batch, training=True)
+                batch = self.fileter_compleated_state(batch)
+                loss_actor, loss_critic = self.compute_loss(batch, training=True)
 
-            cumulated_metrics["actor_loss"] = (
-                cumulated_metrics.setdefault("actor_loss", 0) + loss_actor
-            )
-            cumulated_metrics["critic_loss"] = (
-                cumulated_metrics.setdefault("critic_loss", 0) + loss_critic
-            )
-
-            if step_number % self.target_update_frequency == 0:
-                self.update_target_networks(tau=self.tau)
+                if step_number % self.target_update_frequency == 0:
+                    self.update_target_networks(tau=self.tau)

tests/test_algorithms.py
@@ -274,132 +274,129 @@ class TestD4PG(unittest.TestCase):
         for name, param in self.d4pg.critic.named_parameters():
             assert param.grad is None, f"Gradient not flowing in critic for: {name}"
 
-    # def test_train_d4pg(self):
-    #     logger.info("Testing train_d4pg")
-    #     grid_size = 30
-    #     color_values = 11
-    #     max_steps = torch.randint(5, 100, size=(1,)).item()
+    def test_train_d4pg(self):
+        logger.info("Testing train_d4pg")
+        grid_size = 30
+        color_values = 11
+        max_steps = torch.randint(5, 100, size=(1,)).item()
 
-    #     # Create an instance of the ArcBatchGridEnv
-    #     logger.info("Creating ArcBatchGridEnv")
-    #     env = ArcBatchGridEnv(grid_size, color_values)
-    #     env = PixelAwareRewardWrapper(env)
+        # Create an instance of the ArcBatchGridEnv
+        logger.info("Creating ArcBatchGridEnv")
+        env = ArcBatchGridEnv(grid_size, color_values)
+        env = PixelAwareRewardWrapper(env)
 
-    #     # Create an instance of the ArcDataset
-    #     logger.info("Creating ArcDataset")
-    #     dataset = ArcDataset(
-    #         arc_dataset_dir="rlarcworld/dataset/training",
-    #         keep_in_memory=True,
-    #         transform=ArcSampleTransformer(
-    #             (grid_size, grid_size), examples_stack_dim=10
-    #         ),
-    #     )
-    #     train_samples = DataLoader(dataset=dataset, batch_size=self.batch_size)
+        # Create an instance of the ArcDataset
+        logger.info("Creating ArcDataset")
+        dataset = ArcDataset(
+            arc_dataset_dir="rlarcworld/dataset/training",
+            keep_in_memory=True,
+            transform=ArcSampleTransformer(
+                (grid_size, grid_size), examples_stack_dim=10
+            ),
+        )
+        train_samples = DataLoader(dataset=dataset, batch_size=self.batch_size)
 
-    #     logger.info("Training D4PG")
-    #     self.d4pg.train_d4pg(
-    #         env,
-    #         train_samples,
-    #         batch_size=self.batch_size,
-    #         max_steps=max_steps,
-    #     )
+        logger.info("Training D4PG")
+        self.d4pg.fit(
+            max_steps=max_steps,
+        )
 
-    # def test_train_d4pg_with_replay_buffer(self):
-    #     logger.info("Testing train_d4pg with replay buffer")
-    #     grid_size = 30
-    #     color_values = 11
-    #     max_steps = torch.randint(5, 100, size=(1,)).item()
+    def test_train_d4pg_with_replay_buffer(self):
+        logger.info("Testing train_d4pg with replay buffer")
+        grid_size = 30
+        color_values = 11
+        max_steps = torch.randint(5, 100, size=(1,)).item()
 
-    #     # Create an instance of the ArcBatchGridEnv
-    #     logger.info("Creating ArcBatchGridEnv")
-    #     env = ArcBatchGridEnv(grid_size, color_values)
-    #     env = PixelAwareRewardWrapper(env)
+        # Create an instance of the ArcBatchGridEnv
+        logger.info("Creating ArcBatchGridEnv")
+        env = ArcBatchGridEnv(grid_size, color_values)
+        env = PixelAwareRewardWrapper(env)
 
-    #     # Create an instance of the ArcDataset
-    #     logger.info("Creating ArcDataset")
-    #     dataset = ArcDataset(
-    #         arc_dataset_dir="rlarcworld/dataset/training",
-    #         keep_in_memory=True,
-    #         transform=ArcSampleTransformer(
-    #             (grid_size, grid_size), examples_stack_dim=10
-    #         ),
-    #     )
-    #     train_samples = DataLoader(dataset=dataset, batch_size=self.batch_size)
-    #     self.d4pg.replay_buffer = TensorDictReplayBuffer(
-    #         storage=LazyTensorStorage(self.batch_size),
-    #         sampler=PrioritizedSampler(
-    #             max_capacity=self.batch_size,
-    #             alpha=1.0,
-    #             beta=1.0,
-    #             max_priority_within_buffer=True,
-    #         ),
-    #         priority_key="priority",
-    #     )
-    #     self.d4pg.train_d4pg(
-    #         env,
-    #         train_samples,
-    #         batch_size=self.batch_size,
-    #         max_steps=max_steps,
-    #     )
+        # Create an instance of the ArcDataset
+        logger.info("Creating ArcDataset")
+        dataset = ArcDataset(
+            arc_dataset_dir="rlarcworld/dataset/training",
+            keep_in_memory=True,
+            transform=ArcSampleTransformer(
+                (grid_size, grid_size), examples_stack_dim=10
+            ),
+        )
+        train_samples = DataLoader(dataset=dataset, batch_size=self.batch_size)
+        self.d4pg.replay_buffer = TensorDictReplayBuffer(
+            storage=LazyTensorStorage(self.batch_size),
+            sampler=PrioritizedSampler(
+                max_capacity=self.batch_size,
+                alpha=1.0,
+                beta=1.0,
+                max_priority_within_buffer=True,
+            ),
+            priority_key="priority",
+        )
+        self.d4pg.train_d4pg(
+            env,
+            train_samples,
+            batch_size=self.batch_size,
+            max_steps=max_steps,
+        )
 
-    # def test_train_d4pg_with_n_step(self):
-    #     logger.info("Testing train_d4pg with n_steps")
-    #     grid_size = 30
-    #     color_values = 11
-    #     max_steps = torch.randint(5, 100, size=(1,)).item()
-    #     n_steps = torch.randint(3, max_steps, size=(1,)).item()
-    #     gamma = 0.99
+    def test_train_d4pg_with_n_step(self):
+        logger.info("Testing train_d4pg with n_steps")
+        grid_size = 30
+        color_values = 11
+        max_steps = torch.randint(5, 100, size=(1,)).item()
+        n_steps = torch.randint(3, max_steps, size=(1,)).item()
+        gamma = 0.99
 
-    #     # Create an instance of the ArcBatchGridEnv
-    #     logger.info("Creating ArcBatchGridEnv")
-    #     env = ArcBatchGridEnv(grid_size, color_values, n_steps=n_steps, gamma=gamma)
-    #     env = PixelAwareRewardWrapper(env, n_steps=n_steps, gamma=gamma)
+        # Create an instance of the ArcBatchGridEnv
+        logger.info("Creating ArcBatchGridEnv")
+        env = ArcBatchGridEnv(grid_size, color_values, n_steps=n_steps, gamma=gamma)
+        env = PixelAwareRewardWrapper(env, n_steps=n_steps, gamma=gamma)
 
-    #     # Create an instance of the ArcDataset
-    #     logger.info("Creating ArcDataset")
-    #     dataset = ArcDataset(
-    #         arc_dataset_dir="rlarcworld/dataset/training",
-    #         keep_in_memory=True,
-    #         transform=ArcSampleTransformer(
-    #             (grid_size, grid_size), examples_stack_dim=10
-    #         ),
-    #     )
-    #     train_samples = DataLoader(dataset=dataset, batch_size=self.batch_size)
-    #     replay_buffer = TensorDictReplayBuffer(
-    #         storage=LazyTensorStorage(self.batch_size),
-    #         sampler=PrioritizedSampler(
-    #             max_capacity=self.batch_size,
-    #             alpha=1.0,
-    #             beta=1.0,
-    #             max_priority_within_buffer=True,
-    #         ),
-    #         priority_key="priority",
-    #     )
-    #     num_atoms = {"pixel_wise": 50, "binary": 3, "n_reward": 50 * n_steps}
-    #     v_min = {"pixel_wise": -40, "binary": 0, "n_reward": -40 * n_steps}
-    #     v_max = {"pixel_wise": 2, "binary": 1, "n_reward": 2 * n_steps}
-    #     critic = ArcCriticNetwork(
-    #         size=self.grid_size,
-    #         color_values=self.color_values,
-    #         num_atoms=num_atoms,
-    #         v_min=v_min,
-    #         v_max=v_max,
-    #         test=True,
-    #     )
+        # Create an instance of the ArcDataset
+        logger.info("Creating ArcDataset")
+        dataset = ArcDataset(
+            arc_dataset_dir="rlarcworld/dataset/training",
+            keep_in_memory=True,
+            transform=ArcSampleTransformer(
+                (grid_size, grid_size), examples_stack_dim=10
+            ),
+        )
+        train_samples = DataLoader(dataset=dataset, batch_size=self.batch_size)
+        replay_buffer = TensorDictReplayBuffer(
+            storage=LazyTensorStorage(self.batch_size),
+            sampler=PrioritizedSampler(
+                max_capacity=self.batch_size,
+                alpha=1.0,
+                beta=1.0,
+                max_priority_within_buffer=True,
+            ),
+            priority_key="priority",
+        )
+        num_atoms = {"pixel_wise": 50, "binary": 3, "n_reward": 50 * n_steps}
+        v_min = {"pixel_wise": -40, "binary": 0, "n_reward": -40 * n_steps}
+        v_max = {"pixel_wise": 2, "binary": 1, "n_reward": 2 * n_steps}
+        critic = ArcCriticNetwork(
+            size=self.grid_size,
+            color_values=self.color_values,
+            num_atoms=num_atoms,
+            v_min=v_min,
+            v_max=v_max,
+            test=True,
+        )
 
-    #     d4pg = D4PG(
-    #         actor=self.actor,
-    #         critic=critic,
-    #         n_steps=n_steps,
-    #         gamma=gamma,
-    #         replay_buffer=replay_buffer,
-    #     )
-    #     d4pg.train_d4pg(
-    #         env,
-    #         train_samples,
-    #         batch_size=self.batch_size,
-    #         max_steps=max_steps,
-    #     )
+        d4pg = D4PG(
+            actor=self.actor,
+            critic=critic,
+            n_steps=n_steps,
+            gamma=gamma,
+            replay_buffer=replay_buffer,
+        )
+        d4pg.train_d4pg(
+            env,
+            train_samples,
+            batch_size=self.batch_size,
+            max_steps=max_steps,
+        )
 
 
 if __name__ == "__main__":

tests/test_networks.py
@@ -97,26 +97,29 @@ class ArcNetworksTest(unittest.TestCase):
                 dc.pop(key)
                 network.input_val(dc)
 
-        action_probs = {
-            "x_location": torch.softmax(torch.randn(self.batch_size, 30), dim=-1),
-            "y_location": torch.softmax(torch.randn(self.batch_size, 30), dim=-1),
-            "color_values": torch.softmax(torch.randn(self.batch_size, 11), dim=-1),
-            "submit": torch.softmax(torch.randn(self.batch_size, 2), dim=-1),
-        }
-        best_next_action = torch.cat(
-            [torch.argmax(x, dim=-1).unsqueeze(-1) for x in action_probs.values()],
-            dim=-1,
-        ).float()
+        action_probs = TensorDict(
+            {
+                "x_location": torch.softmax(torch.randn(self.batch_size, 30), dim=-1),
+                "y_location": torch.softmax(torch.randn(self.batch_size, 30), dim=-1),
+                "color_values": torch.softmax(torch.randn(self.batch_size, 11), dim=-1),
+                "submit": torch.softmax(torch.randn(self.batch_size, 2), dim=-1),
+            }
+        )
 
         # numeric stability test
+        to_zero = []
         for key in input_sample.keys():
             if torch.rand(1).item() < 0.2:
                 input_sample[key] = input_sample[key] * 0.0
-        if torch.rand(1).item() < 0.1:
-            best_next_action = best_next_action * 0.0
+                to_zero.append(key)
+        for key in action_probs.keys():
+            if torch.rand(1).item() < 0.2:
+                action_probs[key] = action_probs[key] * 0.0
+                to_zero.append(key)
+
         # Forward pass
         org_sample = input_sample.clone()
-        output = network(input_sample, action=best_next_action)
+        output = network(input_sample, action=action_probs)
 
         # Validate not inplace changes to input
         torch.testing.assert_close(input_sample, org_sample)
@@ -126,7 +129,7 @@ class ArcNetworksTest(unittest.TestCase):
 
         # Define a loss function and an optimizer
         criterion = torch.nn.KLDivLoss(reduction="batchmean")
-        optimizer = torch.optim.Adam(network.parameters(), lr=0.001)
+        optimizer = torch.optim.RMSprop(network.parameters())
 
         # Create dummy target tensors
         target = TensorDict(
@@ -166,14 +169,16 @@ class ArcNetworksTest(unittest.TestCase):
         optimizer.zero_grad()
         loss.backward()
         for name, param in network.named_parameters():
+            input_key = name.split(".")[1]
             if param.grad is None:
                 raise ValueError(
                     f"Gradient not flowing in ArcCriticNetwork for: {name}"
                 )
-            else:
-                print(name, param.grad.abs().sum())
-                assert torch.all(
-                    param.grad.abs().sum() > 0
+            elif input_key not in to_zero:
+                assert (
+                    not torch.all(param.grad.abs().sum() == 0)
+                    or torch.all(input_sample.get(key, torch.tensor(0)) == 0)
+                    or torch.all(action_probs.get(key, torch.tensor(0)) == 0)
                 ), f"Gradient of zero for ArcCriticNetwork for: {name}"
         optimizer.step()
 
@@ -212,7 +217,6 @@ class ArcNetworksTest(unittest.TestCase):
                 "terminated": torch.randint(0, 2, size=(self.batch_size, 1)).float(),
             }
         )
-
         network.input_val(input_sample)
         # Bad input
         for key in input_sample.keys():
@@ -222,9 +226,12 @@ class ArcNetworksTest(unittest.TestCase):
                 network.input_val(dc)
 
         # numeric stability test
+        to_zero = []
         for key in input_sample.keys():
             if torch.rand(1).item() < 0.2:
                 input_sample[key] = input_sample[key] * 0.0
+                print("Actor Input To Zero:", key)
+                to_zero.append(key)
         # Forward pass
         org_sample = input_sample.clone()
         output = network(input_sample)
@@ -234,7 +241,7 @@ class ArcNetworksTest(unittest.TestCase):
 
         # Define a loss function and an optimizer
         criterion = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(network.parameters(), lr=0.001)
+        optimizer = torch.optim.RMSprop(network.parameters())
 
         # Create dummy target tensors
         target = TensorDict(
@@ -274,11 +281,12 @@ class ArcNetworksTest(unittest.TestCase):
         optimizer.zero_grad()
         loss.backward()
         for name, param in network.named_parameters():
+            input_key = name.split(".")[1]
             if param.grad is None:
                 raise ValueError(f"Gradient not flowing in ArcActorNetwork for: {name}")
-            else:
-                assert torch.all(
-                    param.grad.abs().sum() > 0
+            elif input_key not in to_zero:
+                assert not torch.all(param.grad.abs().sum() == 0) or torch.all(
+                    input_sample.get(key, torch.tensor(0)) == 0
                 ), f"Gradient of zero for ArcActorNetwork for: {name}"
         optimizer.step()
         return loss