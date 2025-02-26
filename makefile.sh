0a27b6d3752a5aaf5b7e5ef841872c5803c93feb
Andres Felipe Tello Urrea <andresfe.t.u@gmail.com>
2025-02-25 20:08:50 -0500

    build: Base image from google prebuilt continers

--- makefile.sh
+++ makefile.sh
@@ -1,4 +1,5 @@
 # Push current image
+source devops/.env
 docker buildx build -t ${IMAGE_NAME}:${IMAGE_TAG} --file devops/Dockerfile . --platform=linux/amd64
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${GCP_TRAIN_IMAGE}
 docker push ${GCP_TRAIN_IMAGE}
